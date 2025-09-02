9290408a8e34131b628d7331a4ee85ff175a9d14
R.B <110852196+MRFAKE2002@users.noreply.github.com>
2025-09-02 12:04:50 +0330

    Make Navbar Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,10 +8,12 @@ import Stack from "@/components/Stack";
 import Services from "@/components/Services";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <>
+      <Navbar />
       <Hero />
       <LogoAnimation />
       <About />
